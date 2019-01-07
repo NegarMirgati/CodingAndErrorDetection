1558666c7822071cdc64c90b1cf304c0f1c929d7
NegarMirgati <negar.nbj95@gmail.com>
2019-01-08 02:01:57 +0330

    create classes

--- Convolutional.py
+++ Convolutional.py
@@ -1,85 +1,90 @@
-def moveOnMachine(state, input) :
-    nextState = 0
-    output = ""
-    if(state == 0 and input == "0"): 
+class Convolutional :
+    def moveOnMachine(self, state, input) :
         nextState = 0
-        output = "00"
-    elif(state == 0 and input == "1"): 
-        nextState = 2
-        output = "11"
-    elif(state == 1 and input == "0") : 
-        nextState = 0
-        output = "10"
-    elif(state == 1 and input == "1") : 
-        nextState = 2
-        output = "01"
-    elif(state == 2 and input == "0") : 
-        nextState = 1
-        output = "11"
-    elif(state == 2 and input == "1") : 
-        nextState = 3
-        output = "00"
-    elif(state == 3 and input == "0") :
-        nextState = 1
-        output = "01" 
-    else: 
-        nextState = 3
-        output = "10" 
-    return (nextState, output)
+        output = ""
+        if(state == 0 and input == "0"): 
+            nextState = 0
+            output = "00"
+        elif(state == 0 and input == "1"): 
+            nextState = 2
+            output = "11"
+        elif(state == 1 and input == "0") : 
+            nextState = 0
+            output = "10"
+        elif(state == 1 and input == "1") : 
+            nextState = 2
+            output = "01"
+        elif(state == 2 and input == "0") : 
+            nextState = 1
+            output = "11"
+        elif(state == 2 and input == "1") : 
+            nextState = 3
+            output = "00"
+        elif(state == 3 and input == "0") :
+            nextState = 1
+            output = "01" 
+        else: 
+            nextState = 3
+            output = "10" 
+        return (nextState, output)
       
 
-def encode(input):
-    i = 0
-    state = 0
-    ans = ''
-    while(i < len(input)):
-        state, output = moveOnMachine(state, input[i])
-        ans = ans + output
-        i = i + 1
-    return ans
+    def encode(self, input):
+        i = 0
+        state = 0
+        ans = ''
+        while(i < len(input)):
+            state, output = self.moveOnMachine(state, input[i])
+            ans = ans + output
+            i = i + 1
+        return ans
 
-def decode(input):
-    currentPM = [None] * 4
-    nextPM = [None] * 4
-    currentPM[0] = 0
-    currentPM[1] = float("inf")
-    currentPM[2] = float("inf")
-    currentPM[3] = float("inf")
+    def decode(self, input):
+        currentPM = [None] * 4
+        nextPM = [None] * 4
+        currentPM[0] = 0
+        code = [[0 for x in xrange(len(input)/2)] for y in xrange(4)]
+        currentPM[1] = float("inf")
+        currentPM[2] = float("inf")
+        currentPM[3] = float("inf")
 
-    i = 0
-    while(i < len(input)) :
-        str = input[i : i + 2]
-        if(str == '00') :
-            nextPM[0] = min(currentPM[0], currentPM[1] + 1)
-            nextPM[1] = min(currentPM[2] + 2, currentPM[3] + 1)
-            nextPM[2] = min(currentPM[0] + 2, currentPM[1] + 1)
-            nextPM[3] = min(currentPM[2], currentPM[3] + 1)
-        elif(str == '01') :
-            nextPM[0] = min(currentPM[0] + 1, currentPM[1] + 2)
-            nextPM[1] = min(currentPM[2] + 1, currentPM[3])
-            nextPM[2] = min(currentPM[0] + 1, currentPM[1])
-            nextPM[3] = min(currentPM[2] + 1, currentPM[3] + 2)
-        elif(str == '10') :
-            nextPM[0] =  min(currentPM[0] + 1, currentPM[1])
-            nextPM[1] =  min(currentPM[2] + 1, currentPM[3] + 2)
-            nextPM[2] =  min(currentPM[0] + 1, currentPM[1] + 2)
-            nextPM[3] =  min(currentPM[2] + 1, currentPM[3])
-        else:
-            nextPM[0] = min(currentPM[0] + 2, currentPM[1] + 1)
-            nextPM[1] = min(currentPM[2], currentPM[3] + 1)
-            nextPM[2] = min(currentPM[0], currentPM[1] + 1)
-            nextPM[3] = min(currentPM[2] + 2, currentPM[3] + 1)
-        i = i + 2
-        currentPM = nextPM
+        i = 0
+        while(i < len(input)) :
+            str = input[i : i + 2]
+            if(str == '00') :
+                nextPM[0] = min(currentPM[0], currentPM[1] + 1)
+                nextPM[1] = min(currentPM[2] + 2, currentPM[3] + 1)
+                nextPM[2] = min(currentPM[0] + 2, currentPM[1] + 1)
+                nextPM[3] = min(currentPM[2], currentPM[3] + 1)
+
+            elif(str == '01') :
+                nextPM[0] = min(currentPM[0] + 1, currentPM[1] + 2)
+                nextPM[1] = min(currentPM[2] + 1, currentPM[3])
+                nextPM[2] = min(currentPM[0] + 1, currentPM[1])
+                nextPM[3] = min(currentPM[2] + 1, currentPM[3] + 2)
+            elif(str == '10') :
+                nextPM[0] =  min(currentPM[0] + 1, currentPM[1])
+                nextPM[1] =  min(currentPM[2] + 1, currentPM[3] + 2)
+                nextPM[2] =  min(currentPM[0] + 1, currentPM[1] + 2)
+                nextPM[3] =  min(currentPM[2] + 1, currentPM[3])
+            else:
+                nextPM[0] = min(currentPM[0] + 2, currentPM[1] + 1)
+                nextPM[1] = min(currentPM[2], currentPM[3] + 1)
+                nextPM[2] = min(currentPM[0], currentPM[1] + 1)
+                nextPM[3] = min(currentPM[2] + 2, currentPM[3] + 1)
+
+            i = i + 2
+            currentPM = nextPM
     
-    print(currentPM)
-
+        print(currentPM)
+        print(code)
 
 def main() :
+    c = Convolutional();
     input = raw_input("Enter input : ")
-    code = encode(input)
+    code = c.encode(input)
     print('code= ', code)
-    decoded = decode(code)
+    decoded = c.decode(code)
     print('decode = ', decoded)
 
 if __name__== "__main__":
--- Huffman.py
+++ Huffman.py
@@ -4,7 +4,6 @@ import numpy as np
 import operator
 from Queue import PriorityQueue
 
-
 class HuffmanNode(object):
     def __init__(self, left = None, right = None, str = None, value = None):
         self.left = left
@@ -27,63 +26,63 @@ class HuffmanNode(object):
     
     def setCode(self, code):
         self.code = code
-    
-def create_tree(frequencies): 
-    while len(frequencies) > 1:       
-        indexl, valuel = min(enumerate(frequencies), key=operator.itemgetter(1))
-        frequencies = frequencies[:indexl] + frequencies[indexl+1 :]
-        indexr, valuer = min(enumerate(frequencies), key=operator.itemgetter(1))
-        frequencies = frequencies[:indexr] + frequencies[indexr+1 :]
-        value = valuel[0] + valuer[0]
-        str = valuel[1].getStr() + valuer[1].getStr()
-        node = HuffmanNode(valuel, valuer, str, value)
-        frequencies.append((value, node))
 
-    return frequencies  
+class HuffmanCoding :    
+    def create_tree(self, frequencies): 
+        while len(frequencies) > 1:       
+            indexl, valuel = min(enumerate(frequencies), key=operator.itemgetter(1))
+            frequencies = frequencies[:indexl] + frequencies[indexl+1 :]
+            indexr, valuer = min(enumerate(frequencies), key=operator.itemgetter(1))
+            frequencies = frequencies[:indexr] + frequencies[indexr+1 :]
+            value = valuel[0] + valuer[0]
+            str = valuel[1].getStr() + valuer[1].getStr()
+            node = HuffmanNode(valuel, valuer, str, value)
+            frequencies.append((value, node))
 
-def createCodes(root) :
-    code = root.getCode()
-    children = root.getChildren()
-    if(children[0] != None):
-        children[0][1].setCode(code + "0")
-        createCodes(children[0][1])
-    if(children[1] != None):
-        children[1][1].setCode(code + "1")
-        createCodes(children[1][1])
+        return frequencies  
 
-def createDicts(root, dict_encode, dict_decode) :
-    if(len(root.getStr()) == 1): 
-        print(root.getStr(), root.getCode())
-        dict_encode[root.getStr()] = root.getCode()
-        dict_decode[root.getCode()] = root.getStr()
+    def createCodes(self, root) :
+        code = root.getCode()
+        children = root.getChildren()
+        if(children[0] != None):
+            children[0][1].setCode(code + "0")
+            self.createCodes(children[0][1])
+        if(children[1] != None):
+            children[1][1].setCode(code + "1")
+            self.createCodes(children[1][1])
 
-    children = root.getChildren()
-    if(children[0] != None):
-        createDicts(children[0][1], dict_encode, dict_decode)
-    if(children[1] != None):
-        createDicts(children[1][1], dict_encode, dict_decode)
+    def createDicts(self, root, dict_encode, dict_decode) :
+        if(len(root.getStr()) == 1): 
+            dict_encode[root.getStr()] = root.getCode()
+            dict_decode[root.getCode()] = root.getStr()
 
-def encode(input, dict):
-    output = ''
-    for i in range(0, len(input)) :
-        output = output + dict[input[i]]
-    return output
+        children = root.getChildren()
+        if(children[0] != None):
+            self.createDicts(children[0][1], dict_encode, dict_decode)
+        if(children[1] != None):
+            self.createDicts(children[1][1], dict_encode, dict_decode)
+
+    def encode(self, input, dict):
+        output = ''
+        for i in range(0, len(input)) :
+            output = output + dict[input[i]]
+        return output
         
-def decode(input, dict):
-    found = False
-    code = ''
-    ans = ''
-    i = 0
-    while(i <= len(input)- 1): 
-        code = ''
+    def decode(self, input, dict):
         found = False
-        while(found == False):
-            code = code + input[i]
-            i = i + 1
-            if(dict.has_key(code)):
-                ans = ans + dict[code]
-                found = True
-    return ans
+        code = ''
+        ans = ''
+        i = 0
+        while(i <= len(input)- 1): 
+            code = ''
+            found = False
+            while(found == False):
+                code = code + input[i]
+                i = i + 1
+                if(dict.has_key(code)):
+                    ans = ans + dict[code]
+                    found = True
+        return ans
 
 def main() :
     mat = scipy.io.loadmat('freq.mat')
@@ -95,17 +94,18 @@ def main() :
         node = HuffmanNode(None, None, chr(i), freqs[j][0])
         freqsList.append((freqs[j][0], node))
 
-    tree = create_tree(freqsList)
+    h = HuffmanCoding() 
+    tree = h.create_tree(freqsList)
     root = tree[0][1]
     root.setCode("")
-    createCodes(root)
+    h.createCodes(root)
     dict_encode = {}
     dict_decode = {}
-    createDicts(root, dict_encode, dict_decode)
+    h.createDicts(root, dict_encode, dict_decode)
     input = raw_input("Enter input : ")
-    code = (encode(input, dict_encode))
+    code = (h.encode(input, dict_encode))
     print('code= ', code)
-    raw = decode(code, dict_decode)
+    raw = h.decode(code, dict_decode)
     print('decode= ', raw)
 
 if __name__== "__main__":
